3de91ad2cdf351f7830db5efc5e21a0ce02cacbf
Gautham2980 <gautham4816@gmail.com>
2023-07-19 20:21:25 +0530

    mutliplicationtable

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var a=prompt("which number multipication table");
+var d=prompt("enter limit of multipication");
+var b=1;
+var c;
+while(b<=d)
+      {
+		c=b*a;
+		document.write(b+"x"+a+"="+c+"<br>");
+		b++;
+	
+      }
